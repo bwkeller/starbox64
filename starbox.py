Added a flag for units.  Also fixed units for mass, since there will not *always* be exactly N^3 particles with HCP.

starbox.py
@@ -14,14 +14,16 @@ MYR_IN_CGS = 3.154e13
 K_B = 1.381e-16
 ZSOL = 0.013
 
-def calc_mass(args):
+def calc_mass(args, N):
     if args.number_density:
+        print("Using Number Density: %g H/cc" % args.number_density)
         mass = MP_IN_CGS * args.number_density * np.power(PC_IN_CGS *
-                args.boxSize, 3) / args.boxN 
+                args.boxSize, 3) / N
     elif args.mass_density:
-        mass = MSOL_IN_CGS * args.mass_density * (np.power(args.boxSize, 3) /
-                args.boxN)
+        print("Using Mass Density: %g Msol/pc^3" % args.mass_density)
+        mass = MSOL_IN_CGS * args.mass_density * np.power(args.boxSize, 3) / N
     elif args.gas_mass:
+        print("Using Gas Particle Mass: %g Msol" % args.gas_mass)
         mass = MSOL_IN_CGS * args.gas_mass 
     else:
         raise RuntimeError("Cannot calculate density!")
@@ -40,16 +42,27 @@ if __name__ == "__main__":
             type=str)
     parser.add_argument("-f", "--format", help="""The output format for the initial conditions.
             Currently supported formats are ArepoHDF.""", type=str)
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-n", "--number_density", help="The density of the gas, in H/cc",
+    units = parser.add_mutually_exclusive_group()
+    units.add_argument("-c", "--cgs", help="Code units will be written out in CGS",
+            action='store_true', default=True)
+    units.add_argument("-m", "--msol", help="Code units will be written out MSol, pc, km/s",
+            action='store_true', default=False)
+    mass_den = parser.add_mutually_exclusive_group(required=True)
+    mass_den.add_argument("-n", "--number_density", help="The density of the gas, in H/cc",
             type=float)
-    group.add_argument("-d", "--mass_density", help="The density of the gas, in Msol/pc^3",
+    mass_den.add_argument("-d", "--mass_density", help="The density of the gas, in Msol/pc^3",
             type=float)
-    group.add_argument("-g", "--gas_mass", help="The gas particle mass, in solar masses",
+    mass_den.add_argument("-g", "--gas_mass", help="The gas particle mass, in solar masses",
             type=float)
     args = parser.parse_args()
     
     fname = "box_%d_%3.2e_pc_%3.2e_star.hdf5" % (args.boxN, args.boxSize, args.starMass)
     fname = "test.hdf5"
-    iowriter.write_arepo_HDF(fname, args.boxSize*PC_IN_CGS, calc_mass(args),
-            geometry.hcp(args.boxN), args.starMass*MSOL_IN_CGS)
+    pos = geometry.hcp(args.boxN)
+    gas_mass = calc_mass(args, pos.shape[0])
+    print("Stellar Mass: %g Msol" % args.starMass)
+    print ("Generating Box with %d particles" % pos.shape[0])
+    print("Particle Mass: %g Msol" % gas_mass)
+    if(args.cgs):
+        iowriter.write_arepo_HDF(fname, args.boxSize*PC_IN_CGS, gas_mass,
+                pos, args.starMass*MSOL_IN_CGS)