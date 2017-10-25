f682e6ec8fbdc28a3c0d891e4812f4f218ac12d7
Ben Keller <malzraa@gmail.com>
2017-10-25 12:22:18 +0000

    Refactored HCP generator, much faster now

--- geometry.py
+++ geometry.py
@@ -9,16 +9,17 @@ def hcp(n_side):
     Hexagonal close-pack.  Return x,y,z ndarrays for n_side^3 particles
     in a Hexagonal Close Pack grid.  
     """
-    x = []
-    y = []
-    z = []
-    for i in range(n_side):
-        for j in range(n_side+1):
-            for k in range(n_side+2):
-                x.append((2*i+((j+k) % 2))/float(2*n_side))
-                y.append(np.sqrt(3)*(j+1./3*(k % 2))/float(2*n_side))
-                z.append(2*np.sqrt(6)*k/(3*float(2*n_side)))
+    idx = np.concatenate(np.meshgrid(range(n_side), range(n_side+1), range(n_side+2),
+        indexing='ij')).reshape(3, n_side*(n_side+1)*(n_side+2))
+    i = idx[0]
+    j = idx[1]
+    k = idx[2]
+    x = (2*i+((j+k) % 2))/float(2*n_side)
+    y = np.sqrt(3)*(j+1./3*(k % 2))/float(2*n_side)
+    z = 2*np.sqrt(6)*k/(3*float(2*n_side))
     arr = np.array([x,y,z]).T
     # Shift the HCP lattice to the center of the box
-    arr = arr + np.stack([(1.0 - arr.max(axis=0))/2. for _ in range(arr.shape[0])])
+    edge = (1 - np.array([(2*(n_side-1)+1)/float(2*n_side), np.sqrt(3)*(n_side+1./3)/float(2*n_side),
+            2*np.sqrt(6)*(n_side+1)/(6*float(n_side))]))/2.0
+    arr = arr + np.stack([edge for _ in range(arr.shape[0])])
     return arr
